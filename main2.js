579570c7162b380873b79a014d9ac37237d78b70
alxsandre <alexandr.foret@gmail.com>
2021-05-17 16:09:56 +0200

    main2 test again until chapiter5 finish

---
+++ main2.js
@@ -0,0 +1,147 @@
+//return object
+function createElement(type, props, ...children) {
+    return {
+      type,
+      props: {
+        ...props,
+        children: children.map(child =>
+          typeof child === "object" ? child : createTextElement(child)
+        )
+      }
+    };
+  }
+  
+  function createTextElement(text) {
+    return {
+      type: "TEXT_ELEMENT",
+      props: {
+        nodeValue: text,
+        children: []
+      }
+    };
+  }
+  
+  //create fiber for the dom
+  function createDom(fiber) {
+    const dom =
+      fiber.type == "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(fiber.type);
+    const isProperty = key => key !== "children";
+    Object.keys(fiber.props) //return children or id, class etc.
+      .filter(isProperty) // return all props but no childrens
+      .forEach(name => {
+        dom[name] = fiber.props[name];
+      }); //added props (id, class...) at the dom
+      return dom
+  }
+
+  function commitRoot() {
+      //add nodes to dom
+      commitWork(wipRoot.child);
+      wipRoot = null;
+  }
+
+  function commitWork() {
+      if (!fiber) {
+          return
+      }
+      const domParent = fiber.parent.dom;
+      domParent.appendChild(fiber.dom);
+      commitWork(fiber.child);
+      commitWork(fiber.sibling);
+  }
+  //we set nextUnitOfWork to the root of the fiber tree
+  function render(element, container) {
+    wipRoot = {
+        dom: container,
+        props: {
+            children: [element],
+        },
+    }
+    nextUnitOfWork = wipRoot;
+  }
+
+  //Break the work into small units, after we finish each unit we'll let the browser interrupt the rendering
+  
+  let nextUnitOfWork = null;
+  let wipRoot = null;
+
+  function workLoop(deadline) {
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        )
+        shouldYield = deadline.timeRemaining() < 1 //requestIdleCallback give us deadline parameter to check how much time we have until the browser needs to take control again
+    };
+    if (!nextUnitOfWork && wipRoot) {
+        commitRoot()
+    }
+    requestIdleCallback(workLoop)
+  }
+  requestIdleCallback(workLoop) //action workloop when the main thread is idle
+
+  //performs the work but also returns the next unit of work
+  function performUnitOfWork(fiber) {
+    //add dom node
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    } //If we haven't fiber dom so create fiber to the dom
+
+    //create new fibers
+    const elements = fiber.props.children;
+    let index = 0;
+    let prevSibling = null;
+
+    while (index < elements.length) {
+        const element =  elements[index];
+
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null
+        }
+    }
+
+    //we add it to the fiber tree as a child or as a sibling
+    if (index === 0) {
+        fiber.child = newFiber
+    } else {
+        prevSibling.sibling = newFiber
+        index++
+    }
+
+    // return next unit of work
+    //We try first with the child, then with the sibling, then with with the uncle, and so on
+    if (fiber.child) {
+        return fiber.child
+    }
+    
+    let nextFiber = fiber;
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
+    }
+    
+  }
+
+  const Didact = {
+    createElement,
+    render
+  };
+  
+  /** @jsx Didact.createElement */
+  const element = Didact.createElement(
+    "div",
+    { id: "foo" },
+    Didact.createElement("a", null, "bar"),
+    Didact.createElement("b")
+  )
+  const container = document.getElementById("root");
+  Didact.render(element, container);
+  
+
