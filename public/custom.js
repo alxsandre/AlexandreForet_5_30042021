c510f6fbc92743a00221a42539bd5f025f92b537
alxsandre <alexandr.foret@gmail.com>
2021-06-02 13:24:43 +0200

    static front almost done (without thanks page)

--- public/custom.js
+++ public/custom.js
@@ -1,16 +1,62 @@
- 
+
+
+//add article in the basket
+const numberBasket = document.querySelector('.basket__number')
+let number = 0;
+
+function addArticle() {
+    number = ++number;
+    numberBasket.innerHTML = number;
+}
+
+if ( document.URL.includes("PDP.html") ) {
+    const ctaAddArticle = document.querySelector('.PDP__CTA')
+    ctaAddArticle.addEventListener("click", addArticle);
+}
+
+numberBasket.innerHTML = number;
+
  const createThumbnail = (productInfos) => {
+        const main = document.querySelector('.main__accueil');
+
         //create thumnbail
-        const main = document.querySelector('main');
         const section = document.createElement('section');
         main.appendChild(section);
         const thumnbailStyle = document.createAttribute('class');
         thumnbailStyle.value = "thumnbail";
         section.setAttributeNode(thumnbailStyle);
+        
+        //create link thumnbail
+        const link = document.createElement('a');
+        section.appendChild(link);
+        const href = document.createAttribute('href');
+        href.value = "./pages/PDP.html"
+        link.setAttributeNode(href);
 
+        //create title for thumnbail
+        const h2 = document.createElement('h2');
+        link.appendChild(h2).innerHTML = productInfos.name;
+
+        //create price for thumnbail
+        const span = document.createElement('span');
+        const priceStyle = document.createAttribute('class');
+        priceStyle.value = "price";
+        span.setAttributeNode(priceStyle);
+        let price = productInfos.price;
+        let priceFloat = parseFloat(price);
+        priceFloat = priceFloat * 0.01;
+        priceFloat = priceFloat.toFixed(2) + ' €';
+        link.appendChild(span).innerHTML = priceFloat;
+        
         //create img for thumnbail
+        const div = document.createElement('div');
+        link.appendChild(div);
+        const imgContainer = document.createAttribute('class');
+        imgContainer.value = "img__container";
+        div.setAttributeNode(imgContainer);
+
         const img = document.createElement('img');
-        section.appendChild(img);
+        div.appendChild(img);
 
         const imgUrl = document.createAttribute('src');
         imgUrl.value = productInfos.imageUrl;
@@ -20,17 +66,11 @@
         imgDescription.value = productInfos.description;
         img.setAttributeNode(imgDescription);
 
-        //create title for thumnbail
-        const h2 = document.createElement('h2');
-        section.appendChild(h2).innerHTML = productInfos.name;
-
-        //create price for thumnbail
-        const span = document.createElement('span');
-        section.appendChild(span).innerHTML = productInfos.price;
-
+        /*
         //create description for thumnbail
         const para = document.createElement('p');
         section.appendChild(para).innerHTML = productInfos.description;
+        */
     };
 
 
