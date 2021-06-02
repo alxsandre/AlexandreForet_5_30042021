f122cb5ca6d399b6a7cc807e8290417ca5758b80
Alexandre.foret <alexandreforet5@gmail.com>
2021-06-02 18:14:53 +0200

    localStore implementation for the basket => ok

--- public/custom.js
+++ public/custom.js
@@ -1,20 +1,66 @@
 
+/*class Article {
+    constructor(listArticles){
+        listArticles && Object.assign(this, listArticles)
+    }
+
+    getFormatedPrice() {
+        let priceFloat = parseFloat(this.price);
+        priceFloat = priceFloat * 0.01;
+        priceFloat = priceFloat.toFixed(2) + ' €';
+        return priceFloat;
+    }
+}
+
+class ArticleManager {
+    constructor(listArticle){
+        this.listArticle = listArticle;
+    }
+}*/
+
+/*
+function addBasket(number) {
+    let listBasket = getBasket();
+    listBasket.push(number);
+    saveBasket(listBasket);
+}
+
+function getBasket() {
+    let listBasket = localStorage.getItem("listBasket");
+    if (listBasket == null) {
+        return [];
+    } else {
+        return JSON.parse(listBasket);
+    }
+}
+
+function saveBasket(listBasket) {
+    localStorage.setItem("listBasket", JSON.stringify(listBasket));
+}
+*/
 
 //add article in the basket
 const numberBasket = document.querySelector('.basket__number')
 let number = 0;
 
-function addArticle() {
-    number = ++number;
-    numberBasket.innerHTML = number;
+function addNumberBasket() {
+    number = Number(localStorage.getItem("numberInBasket"));
+    ++number;
+    localStorage.setItem("numberInBasket", number);
+    numberBasket.innerHTML = localStorage.getItem("numberInBasket");
 }
 
-if ( document.URL.includes("PDP.html") ) {
+if (document.URL.includes("PDP.html")) {
     const ctaAddArticle = document.querySelector('.PDP__CTA')
-    ctaAddArticle.addEventListener("click", addArticle);
+    ctaAddArticle.addEventListener("click", addNumberBasket);
 }
 
-numberBasket.innerHTML = number;
+if (localStorage.getItem("numberInBasket")) {
+    numberBasket.innerHTML = localStorage.getItem("numberInBasket");
+}
+
+
+
 
  const createThumbnail = (productInfos) => {
         const main = document.querySelector('.main__accueil');
