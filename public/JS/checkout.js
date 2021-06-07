d50b2f103c6341a216d190b68f262bcc80d7da4f
alxsandre <alexandr.foret@gmail.com>
2021-06-07 10:43:13 +0200

    get data localstorage adn push it for the checkout -> done; we have to add the colors and total price in the checkout

--- public/JS/checkout.js
+++ public/JS/checkout.js
@@ -2,38 +2,41 @@ import './cart.js';
 
 const anchorProduct = document.querySelector('article');
 
+//get list of product and parse it to become an array
 let listProduct = localStorage.getItem("listProduct");
 listProduct = JSON.parse(listProduct);
 
-for(const idProduct of listProduct) {
-   // console.log(idProduct);
-
-    
-}
-
+//counts number of products for each
 var counts = [];
 listProduct.forEach( x => { counts[x] = (counts[x] || 0)+1; });
 
+//get products id
+let productsId = Object.keys(counts); 
 
-console.log(counts);
+//Put products Id in the URL to get data
+for (let i = 0; i < productsId.length; i++) {
+  
+    if (document.URL.includes("checkout.html")) {
 
-let nameProductIntoCart = Array.from(new Set(listProduct));
-console.table(nameProductIntoCart);
-
-
-const iterator = counts.keys();
-for (const key of iterator) {
-  console.log(key);
+        fetch('http://localhost:3000/api/teddies/' + productsId[i])
+            .then(res => {
+                if (res.ok) return res.json()
+                throw new Error('Dommage !')
+            })
+            .then(product => {
+                anchorProduct.innerHTML += `<article class="containernospace">
+                                                <div class="img__container">
+                                                    <img src="${product.imageUrl}" alt="${product.description}">
+                                                </div>
+                                                <div class="checkout__container">
+                                                    <h2>${product.name}</h2>
+                                                    <span class="checkout__price">${product.price}</span>
+                                                    <div class="checkout__colors">${product.colors}</div>
+                                                    <span>Quantité : ${counts[productsId[i]]}</span>
+                                                </div>
+                                            </article>`;
+                
+            })
+            .catch(err => console.error(err));    
+    }
 }
-
-
-anchorProduct.innerHTML = `<article class="containernospace">
-    <div class="img__container">
-        <img src="http://localhost:3000/images/teddy_1.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.">
-    </div>
-    <div class="checkout__container">
-        <h2>Norbert</h2>
-        <span class="checkout__price">29.00 €</span>
-        <div class="checkout__colors">colors</div>
-    </div>
-</article>`
