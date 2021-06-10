4636b5cd5efbb67be9fda7ab5a5603583cb87aba
Alexandre.foret <alexandreforet5@gmail.com>
2021-06-10 11:24:38 +0200

    some test for post and the iterable object

--- public/JS/checkout.js
+++ public/JS/checkout.js
@@ -8,12 +8,15 @@ listProduct = JSON.parse(listProduct);
 
 //counts number of products for each
 var counts = [];
-listProduct.forEach( x => { counts[x] = (counts[x] || 0)+1; });
+if(listProduct) {
+    listProduct.forEach( x => { counts[x] = (counts[x] || 0)+1; });
+}
 
 //get products id
 let productsId = Object.keys(counts); 
+let products = productsId;
 
-//Put products Id in the URL to get data
+//Put products Id in the URL API to get data
 for (let i = 0; i < productsId.length; i++) {
   
     if (document.URL.includes("checkout.html")) {
@@ -41,6 +44,20 @@ for (let i = 0; i < productsId.length; i++) {
     }
 }
 
+let testProducts = {
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl: "http://localhost:3000/images/teddy_1.jpg",
+    name: "Norbert",
+    price: 2900,
+    _id: "5be9c8541c9d440000665243"
+}
+
+testProducts = Object.keys(testProducts)
+for (const element of testProducts) {
+    console.log(element);
+  }
+console.log(testProducts.description)
+
 if (document.URL.includes("checkout.html")) {
     document.querySelector('.form')
         .addEventListener("submit",function(e) {
@@ -52,7 +69,6 @@ if (document.URL.includes("checkout.html")) {
             formData.forEach(function(value, key){
                 contact[key] = value;
             });
-            console.log(contact)
             let valid = true;
             for(let input of document.querySelectorAll("form input")) {
                 valid &= input.reportValidity()
@@ -61,7 +77,10 @@ if (document.URL.includes("checkout.html")) {
             if (valid) {
                 fetch('http://localhost:3000/api/teddies/order', {
                     method: 'post',
-                    body: contact
+                    body: {
+                        contact: contact, 
+                        products: testProducts
+                    }
                 })
                 .then(res => res.text())
                 .then(text => console.log(text))
