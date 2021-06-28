abffff96ccf58f3de2b7ba33cd70acbdc3bf6c66
alxsandre <alexandr.foret@gmail.com>
2021-06-28 11:14:09 +0200

    server removed & comment added

--- public/JS/utils.js
+++ public/JS/utils.js
@@ -1,13 +1,17 @@
-//take id in the URL parameter
-//formating price
 export let idUrl;
 export { transformPrice };
 
+//take id in the URL parameter
 if (document.URL.includes("productpage.html")) {
   const actualUrl = new URL(window.location.href);
   idUrl = actualUrl.search.substring(4);
 }
 
+/**
+ * formating price
+ * @param {number} price 
+ * @returns {string} priceFloat
+ */
 function transformPrice(price) {
   let priceFloat = parseFloat(price);
   priceFloat = priceFloat * 0.01;
